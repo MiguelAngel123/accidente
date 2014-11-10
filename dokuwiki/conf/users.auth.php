d389ed25b9178c3906d07819f80998716708983d
MiguelAngel123 <miguel_ancangel@hotmail.com>
2014-11-10 13:54:16 -0400

    nuevo

---
+++ dokuwiki/conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,seperated
+
+
+admin:$1$Vt3VJm1n$0c12kN4sc7zLksnhwLMtw.:Arsenio Castellon:arseniocastellon@gmail.com:admin,user
---
+++ views/footer.php
@@ -0,0 +1,15 @@
+</div>
+<div class="clear"></div>
+</div>
+</div>
+
+<div id="footer">
+    <a href="http://localhost/" target="_blank" class="byLogos" title="Desarrollado por Desarrollador">
+        Desarrollado por Desarrollador    
+    </a>
+</div>
+</div>
+<script type="text/javascript">
+</script>
+</body>
+</html>
